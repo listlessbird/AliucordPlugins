edcafc2c081c024110d002dcdad365d60f48a0ed
listlessbird <124798751+listlessbird@users.noreply.github.com>
2025-04-25 15:24:17 +0530

    init?

---
+++ build.gradle.kts
@@ -0,0 +1,71 @@
+
+
+
+import com.aliucord.gradle.AliucordExtension
+import com.android.build.gradle.BaseExtension
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+        maven("https://maven.aliucord.com/snapshots")
+    }
+    dependencies {
+        classpath("com.aliucord:gradle:main-SNAPSHOT")
+        classpath("com.aliucord:jadb:1.2.1-SNAPSHOT")
+        classpath("com.android.tools.build:gradle:7.0.4")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.aliucord.com/snapshots")
+    }
+}
+
+fun Project.aliucord(configuration: AliucordExtension.() -> Unit) = extensions.getByName<AliucordExtension>("aliucord").configuration()
+
+fun Project.android(configuration: BaseExtension.() -> Unit) = extensions.getByName<BaseExtension>("android").configuration()
+
+subprojects {
+    apply(plugin = "com.android.library")
+    apply(plugin = "com.aliucord.gradle")
+
+
+    aliucord {
+        author("_.swaen", 955296933705044018)
+
+        updateUrl.set("https://raw.githubusercontent.com/listlessbird/AliucordPlugins/builds/updater.json")
+        buildUrl.set("https://raw.githubusercontent.com/listlessbird/AliucordPlugins/builds/%s.zip")
+    }
+
+    android {
+        compileSdkVersion(30)
+
+        defaultConfig {
+            minSdk = 24
+            targetSdk = 30
+        }
+
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_11
+            targetCompatibility = JavaVersion.VERSION_11
+        }
+
+    }
+
+    dependencies {
+        val discord by configurations
+        val compileOnly by configurations
+
+        discord("com.discord:discord:aliucord-SNAPSHOT")
+        compileOnly("com.aliucord:Aliucord:main-SNAPSHOT")
+    }
+}
+
+task<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
