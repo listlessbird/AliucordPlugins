6877c3ecc7838147f150fd8efea0d7a0e297ee2d
listlessbird <124798751+listlessbird@users.noreply.github.com>
2025-12-12 19:45:14 +0530

    add template

---
+++ new.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -e
+
+name="$1"
+[ -z "$name" ] && { >&2 echo "Please specify a name"; exit 1; }
+first_char="$(printf %.1s "$name")"
+[ "$(echo "$first_char"  | tr "[:lower:]" "[:upper:]")" != "$first_char" ] && { >&2 echo "Name must be PascalCase"; exit 1; }
+
+d="$name/src/main/kotlin/com.github.listlessbird.plugins"
+new="$d/$name.kt"
+
+set -x
+# shellcheck disable=SC2039
+shopt -s extglob
+mkdir -p "$name/src"
+# shellcheck disable=SC2039
+cp -r Template/!(build) "$name"
+
+# Rename file
+mv "$d/Template.kt" "$new"
+# Replace "Template" in file
+sed -i -e "s/Template/$name/g" "$new"
+# Change class name
+sed -i "s/Template/$name/" "$new"
+
+# Add to settings.gradle
+echo "include(\":$name\")" | cat - settings.gradle.kts > settings.gradle.new && mv settings.gradle.new settings.gradle.kts
